97d9d18e3e30936b27cf26e072b74c16eb1f691c
KingChaos31 <marytecle756@gmail.com>
2022-04-18 20:33:44 -0700

    Started work on a text adventure where you can switch between 3 rooms.
    1. Learned how to store strings in array and how to address elements
    in the array.
    2. Learned how to gather user input.
    3. Learned how to call member functions (tell strings what to do) for
    example "Right".lower() == "right"
    4. Learned about while loops(user input)
    5. Learned more about if/else and while(control structures)
    6. Did a bit of error checking with the invalid response.
    There's a bug in this where once you leave room 2 you can never go back
    also add q so the player can quit .

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 # Entry point for application
 
+from xmlrpc.client import ResponseError
 from rich.console import Console
 
 if __name__ == "__main__": 
     console = Console()
     console.print("[yellow]potatos [blue]are[/] poggers")
+
+    room_descriptions = [ 
+        "wrrrrrrrrrrrrry",
+        "The room you enter is barbie [green]pink[/] room with a mountain of stuffed animals.", 
+        "The room you enter is [black]black[/] and Gothic with what can only be described as an \"edgy\" teenager energy throughout. "
+    ]
+    current_room = 2
+    done = False
+    while not done:
+        console.print(room_descriptions[current_room])
+        user_input = input("Left or Right? ")
+        user_input = user_input.lower().strip()
+        if user_input == "left" or user_input == "l":
+            current_room = 1
+        elif user_input == "right" or user_input == "r":
+            current_room = 0 
+        else:
+            console.print("[red]Invalid Response!")
